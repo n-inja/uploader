119d2ad37fde2253c3de81aed6a98d5bf34dc449
n-inja <mccreepertakumi@gmail.com>
2018-08-27 19:22:16 +0900

    impl post file, get fileinfo

--- main.go
+++ main.go
@@ -1,5 +1,108 @@
 package main
 
-func main () {
+import (
+	"os"
 
+	"log"
+
+	"net/http"
+
+	"io"
+
+	"./utils"
+	"github.com/gin-gonic/gin"
+	"github.com/rs/xid"
+)
+
+func main() {
+	address := ""
+	if os.Getenv("DATABASE_ADDRESS") != "" {
+		address = os.Getenv("DATABASE_ADDRESS")
+	}
+	err := utils.Open(os.Getenv("DATABASE_USERNAME"), os.Getenv("DATABASE_PASSWORD"), address, os.Getenv("DATABASE_NAME"))
+	if err != nil {
+		log.Fatal(err.Error())
+		return
+	}
+	defer utils.Close()
+
+	router := gin.Default()
+	router.GET("go-uploader/api/v1/files", getFileList)
+	router.GET("go-uploader/api/v1/files/:filename", getFileInfo)
+	router.POST("go-uploader/api/v1/files", postFile)
+	router.DELETE("go-uploader/api/v1/files/:filename")
+	router.PUT("go-uploader/api/v1/files/:filename")
+	router.GET("static/:filename")
+
+	router.Run(":" + os.Getenv("UPLOADER_PORT"))
+}
+
+func getFileList(c *gin.Context) {
+	ID := c.GetHeader("id")
+	if ID != "" {
+		files := utils.GetFileList(ID)
+		c.JSON(http.StatusOK, gin.H{"files": files})
+	} else {
+		c.JSON(http.StatusUnauthorized, gin.H{})
+	}
+}
+
+func getFileInfo(c *gin.Context) {
+	ID := c.GetHeader("id")
+	if ID != "" {
+		file, err := utils.GetFileByName(ID, c.Param("filename"))
+		if err != nil {
+			c.JSON(http.StatusNotFound, gin.H{})
+		} else {
+			c.JSON(http.StatusOK, file)
+		}
+	} else {
+		c.JSON(http.StatusUnauthorized, gin.H{})
+	}
+}
+
+func postFile(c *gin.Context) {
+	ID := c.GetHeader("id")
+	if ID != "" {
+		auth := c.PostForm("auth")
+		if auth != "public" && auth != "internal" && auth != "private" {
+			c.JSON(http.StatusBadRequest, gin.H{})
+			return
+		}
+
+		file, header, err := c.Request.FormFile("file")
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{})
+			return
+		}
+		filename := header.Filename
+		path := os.Getenv("UPLOAD_FILE_PATH") + "/" + xid.New().String()
+		out, err := os.Create(path)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{})
+			return
+		}
+		defer out.Close()
+
+		_, err = io.Copy(out, file)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{})
+		}
+
+		var f utils.File
+		f.UserID = ID
+		f.Name = filename
+		f.Path = path
+		f.Auth = auth
+		err = utils.InsertFileInfo(f)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{})
+			return
+		}
+
+		c.JSON(http.StatusOK, f)
+
+	} else {
+		c.JSON(http.StatusUnauthorized, gin.H{})
+	}
 }
---
+++ utils/db.go
@@ -0,0 +1,100 @@
+package utils
+
+import (
+	"database/sql"
+
+	"errors"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+
+type File struct {
+	Name   string `json:"name" form:"name" binding:"required"`
+	Path   string `json:"path" form:"name" binding:"required"`
+	UserID string `json:"userId" form:"userId" binding:"required"`
+	Auth   string `json:"auth" form:"auth" binding:"required"`
+}
+
+func Open(userName, password, address, databaseName string) error {
+	var err error
+	db, err = sql.Open("mysql", userName+":"+password+"@"+address+"/"+databaseName)
+	if err != nil {
+		return err
+	}
+	return initDB()
+}
+
+func Close() {
+	db.Close()
+}
+
+func initDB() error {
+	rows, err := db.Query("show tables like 'files'")
+	if err != nil {
+		return nil
+	}
+	if !rows.Next() {
+		rows.Close()
+		_, err = db.Exec("create table 'files' (name varchar(256) NOT NULL PRIMARY KEY, path varchar(20) NOT NULL, user_id varchar(32) NOT NULL, auth varchar(10) NOT NULL, index(user_id))")
+		if err != nil {
+			return err
+		}
+	}
+	rows.Close()
+	return nil
+}
+
+func GetFileList(ID string) []File {
+	rows, err := db.Query("select count(*) from files where auth = 'public' OR auth = 'internal' OR auth = 'private' AND user_id = ?", ID)
+	if err != nil {
+		return []File{}[:]
+	}
+	var count int32
+	rows.Next()
+	rows.Scan(&count)
+	rows.Close()
+	var ret [count]File
+
+	rows, err = db.Query("select name, path, user_id, auth from files where auth = 'public' OR auth = 'internal' OR auth = 'private' AND user_id = ?", ID)
+	if err != nil {
+		return []File{}[:]
+	}
+	defer rows.Close()
+
+	for i := 0; rows.Next(); i++ {
+		rows.Scan(&ret[i].Name)
+		rows.Scan(&ret[i].Path)
+		rows.Scan(&ret[i].UserID)
+		rows.Scan(&ret[i].Auth)
+	}
+
+	return ret[:]
+}
+
+func GetFileByName(ID, name string) (File, error) {
+	rows, err := db.Query("select name, path, user_id, auth from files where name = ? and (auth != 'private' or auth = 'private' and user_id = ?)", name, ID)
+	var ret File
+	if err != nil {
+		return ret, err
+	}
+	if !rows.Next() {
+		rows.Close()
+		return ret, errors.New("not found")
+	}
+	rows.Scan(&ret.Name)
+	rows.Scan(&ret.Path)
+	rows.Scan(&ret.UserID)
+	rows.Scan(&ret.Auth)
+	defer rows.Close()
+	return ret, nil
+}
+
+func InsertFileInfo(file File) error {
+	_, err := db.Exec("insert into files values(?, ?, ?, ?)", file.Name, file.Path, file.UserID, file.Auth)
+	if err != nil {
+		return err
+	}
+	return nil
+}

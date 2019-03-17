a2b98b3c4dbe641720883dd3a2ef87d6a98f41b0
n-inja <mccreepertakumi@gmail.com>
2019-03-18 00:01:31 +0900

    gzip

--- main.go
+++ main.go
@@ -1,15 +1,14 @@
 package main
 
 import (
-	"os"
-
 	"log"
+	"os"
+	"os/exec"
+	"strings"
 
 	"net/http"
 
 	"io"
-
-	"fmt"
 
 	"io/ioutil"
 	"path/filepath"
@@ -19,17 +18,18 @@ import (
 	"github.com/rs/xid"
 )
 
+var uploadFilePath string
+
+func init() {
+	uploadFilePath = os.Getenv("UPLOAD_FILE_PATH")
+}
+
 func main() {
-	address := ""
-	if os.Getenv("DATABASE_ADDRESS") != "" {
-		address = os.Getenv("DATABASE_ADDRESS")
-	}
-	err := utils.Open(os.Getenv("DATABASE_USERNAME"), os.Getenv("DATABASE_PASSWORD"), address, os.Getenv("DATABASE_NAME"))
-	if err != nil {
-		log.Fatal(err.Error())
-		return
+	if os.Getenv("UPDATE_MIME") != "" {
+		utils.UpdateMime()
 	}
 
+	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
 	router.GET("go-uploader/api/v1/files", getFileList)
 	router.GET("go-uploader/api/v1/files/:filename", getFileInfo)
@@ -38,98 +38,134 @@ func main() {
 	router.PUT("go-uploader/api/v1/files/:filename", renameFile)
 	router.GET("static/:filename", broadcastFile)
 
-	router.Run(":" + os.Getenv("UPLOADER_PORT"))
+	log.Fatal(router.Run(":" + os.Getenv("UPLOADER_PORT")))
 }
 
 func getFileList(c *gin.Context) {
 	ID := c.GetHeader("id")
-	if ID != "" {
-		files := utils.GetFileList(ID)
-		c.JSON(http.StatusOK, gin.H{"files": files})
-	} else {
+	if ID == "" {
 		c.JSON(http.StatusUnauthorized, gin.H{})
+		return
 	}
+
+	files, err := utils.GetFileList(ID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "database error"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"files": files})
 }
 
 func getFileInfo(c *gin.Context) {
 	ID := c.GetHeader("id")
-	if ID != "" {
-		file, err := utils.GetFileByName(ID, c.Param("filename"))
-		if err != nil {
-			c.JSON(http.StatusNotFound, gin.H{})
-		} else {
-			c.JSON(http.StatusOK, file)
-		}
-	} else {
+	if ID == "" {
 		c.JSON(http.StatusUnauthorized, gin.H{})
+		return
 	}
+
+	file, err := utils.GetFileByName(ID, c.Param("filename"))
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{})
+		return
+	}
+
+	c.JSON(http.StatusOK, file)
 }
 
 func postFile(c *gin.Context) {
 	ID := c.GetHeader("id")
-	if ID != "" {
-		accessLevel := c.PostForm("accessLevel")
-		if accessLevel != "public" && accessLevel != "internal" && accessLevel != "private" {
-			c.JSON(http.StatusBadRequest, gin.H{})
-			return
-		}
-
-		file, header, err := c.Request.FormFile("file")
-		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{})
-			return
-		}
-		filename := header.Filename
-
-		path := xid.New().String()
-
-		if c.PostForm("name") != "" {
-			filename = c.PostForm("name")
-		}
-
-		out, err := os.Create(filepath.Join(os.Getenv("UPLOAD_FILE_PATH"), path))
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{})
-			return
-		}
-		defer out.Close()
-
-		_, err = io.Copy(out, file)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{})
-		}
-
-		var f utils.File
-		f.UserID = ID
-		f.Name = filename
-		f.Path = path
-		f.AccessLevel = accessLevel
-		err = utils.InsertFileInfo(f)
-		if err != nil {
-			fmt.Println(err)
-			c.JSON(http.StatusInternalServerError, gin.H{})
-			return
-		}
-
-		c.JSON(http.StatusOK, f)
-
-	} else {
+	if ID == "" {
 		c.JSON(http.StatusUnauthorized, gin.H{})
+		return
 	}
+
+	accessLevel := c.PostForm("accessLevel")
+	if accessLevel != "public" && accessLevel != "internal" && accessLevel != "private" {
+		c.JSON(http.StatusBadRequest, gin.H{})
+		return
+	}
+
+	file, header, err := c.Request.FormFile("file")
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusBadRequest, gin.H{})
+		return
+	}
+	filename := header.Filename
+
+	path := xid.New().String()
+
+	if c.PostForm("name") != "" {
+		filename = c.PostForm("name")
+	}
+
+	outpath := filepath.Join(uploadFilePath, path)
+	out, err := os.Create(outpath)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{})
+		return
+	}
+
+	_, err = io.Copy(out, file)
+	out.Close()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{})
+		return
+	}
+
+	message, err := exec.Command("bash", "-c", "gzip -c "+outpath+" > "+outpath+".gz").CombinedOutput()
+
+	if err != nil {
+		log.Println(err, message)
+		c.JSON(http.StatusInternalServerError, gin.H{})
+		return
+	}
+
+	var f utils.File
+	f.UserID = ID
+	f.Name = filename
+	f.Path = path
+	f.AccessLevel = accessLevel
+	f.Mime, err = getMime(outpath)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{})
+		return
+	}
+
+	err = utils.InsertFileInfo(f)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{})
+		return
+	}
+
+	c.JSON(http.StatusOK, f)
+}
+
+func getMime(path string) (string, error) {
+	bytes, err := ioutil.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+	return http.DetectContentType(bytes), nil
 }
 
 func deleteFile(c *gin.Context) {
 	ID := c.GetHeader("id")
-	if ID != "" {
-		err := utils.DeleteFile(ID, c.Param("filename"))
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{})
-		} else {
-			c.JSON(http.StatusOK, gin.H{})
-		}
-	} else {
+	if ID == "" {
 		c.JSON(http.StatusUnauthorized, gin.H{})
+		return
 	}
+
+	err := utils.DeleteFile(ID, c.Param("filename"))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{})
 }
 
 type RenamePost struct {
@@ -139,52 +175,64 @@ type RenamePost struct {
 
 func renameFile(c *gin.Context) {
 	ID := c.GetHeader("id")
-
-	if ID != "" {
-		var json RenamePost
-		if c.BindJSON(&json) != nil {
-			c.JSON(http.StatusBadRequest, gin.H{})
-			return
-		}
-		if json.NewAccessLevel != "" && json.NewAccessLevel != "public" && json.NewAccessLevel != "internal" && json.NewAccessLevel != "private" {
-			c.JSON(http.StatusBadRequest, gin.H{})
-			return
-		}
-
-		err := utils.RenameFile(ID, c.Param("filename"), json.NewName, json.NewAccessLevel)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{})
-		} else {
-			c.JSON(http.StatusOK, gin.H{})
-		}
-	} else {
+	if ID == "" {
 		c.JSON(http.StatusUnauthorized, gin.H{})
+		return
 	}
+
+	var json RenamePost
+	if c.BindJSON(&json) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{})
+		return
+	}
+	if json.NewAccessLevel != "" && json.NewAccessLevel != "public" && json.NewAccessLevel != "internal" && json.NewAccessLevel != "private" {
+		c.JSON(http.StatusBadRequest, gin.H{})
+		return
+	}
+
+	err := utils.RenameFile(ID, c.Param("filename"), json.NewName, json.NewAccessLevel)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{})
 }
 
 func broadcastFile(c *gin.Context) {
 	ID := c.GetHeader("id")
 	filename := c.Param("filename")
-	var path string
+	var path, mime string
 	var err error
 
 	if ID != "" {
-		path, err = utils.GetFilePath(ID, filename)
+		path, mime, err = utils.GetFilePath(ID, filename)
 	} else {
-		path, err = utils.GetPublicFilePath(filename)
+		path, mime, err = utils.GetPublicFilePath(filename)
 	}
 	if err != nil {
 		c.JSON(http.StatusNotFound, gin.H{})
 		return
 	}
 
-	bytes, err := ioutil.ReadFile(filepath.Join(os.Getenv("UPLOAD_FILE_PATH"), path))
+	serveFile(c, filepath.Join(uploadFilePath, path), mime)
+}
+
+func serveFile(c *gin.Context, path, mime string) {
+	gzipOK := strings.Contains(c.GetHeader("Accept-Encoding"), "gzip")
+	var bytes []byte
+	var err error
+	if gzipOK {
+		bytes, err = ioutil.ReadFile(path + ".gz")
+		c.Header("Content-Encoding", "gzip")
+	} else {
+		bytes, err = ioutil.ReadFile(path)
+	}
 	if err != nil {
+		log.Println(err)
 		c.JSON(http.StatusInternalServerError, gin.H{})
 		return
 	}
 
-	mimeType := http.DetectContentType(bytes)
-
-	c.Data(http.StatusOK, mimeType, bytes)
+	c.Data(http.StatusOK, mime, bytes)
 }
--- utils/db.go
+++ utils/db.go
@@ -2,6 +2,12 @@ package utils
 
 import (
 	"database/sql"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
 
 	"errors"
 
@@ -15,16 +21,26 @@ type File struct {
 	Path        string `json:"path" form:"name" binding:"required"`
 	UserID      string `json:"userId" form:"userId" binding:"required"`
 	AccessLevel string `json:"accessLevel" form:"accessLevel" binding:"required"`
+	Mime        string `json:"mime" form:"mime"`
 }
 
-func Open(userName, password, address, databaseName string) error {
+func init() {
+	userName := os.Getenv("DATABASE_USERNAME")
+	password := os.Getenv("DATABASE_PASSWORD")
+	address := os.Getenv("DATABASE_ADDRESS")
+	databaseName := os.Getenv("DATABASE_NAME")
 	var err error
-	db, err = sql.Open("mysql", userName+":"+password+"@"+address+"/"+databaseName)
+	db, err = sql.Open("mysql", userName+":"+password+"@tcp("+address+")/"+databaseName)
+
 	if err != nil {
-		return err
+		log.Fatal(err)
 	}
+
 	db.SetMaxIdleConns(0)
-	return initDB()
+	err = initDB()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func Close() {
@@ -38,7 +54,7 @@ func initDB() error {
 	}
 	defer rows.Close()
 	if !rows.Next() {
-		_, err = db.Exec("create table files (name varchar(256) NOT NULL PRIMARY KEY, path varchar(20) NOT NULL, user_id varchar(32) NOT NULL, access_level varchar(10) NOT NULL, index(user_id))")
+		_, err = db.Exec("create table files (name varchar(256) NOT NULL PRIMARY KEY, path varchar(20) NOT NULL, user_id varchar(32) NOT NULL, access_level varchar(10) NOT NULL, mime varchar(32) NOT NULL, index(user_id))")
 		if err != nil {
 			return err
 		}
@@ -47,36 +63,34 @@ func initDB() error {
 	return nil
 }
 
-func GetFileList(ID string) []File {
+func GetFileList(ID string) ([]File, error) {
 	rows, err := db.Query("select count(*) from files where access_level = 'public' OR access_level = 'internal' OR access_level = 'private' AND user_id = ?", ID)
 	if err != nil {
-		return []File{}[:]
+		return nil, err
 	}
 	var count int32
 	rows.Next()
 	rows.Scan(&count)
 	rows.Close()
 
-	if count > 1000 {
-		count = 1000
-	}
-
-	var ret [1000]File
+	files := make([]File, 0)
 
 	rows, err = db.Query("select name, path, user_id, access_level from files where access_level = 'public' OR access_level = 'internal' OR access_level = 'private' AND user_id = ?", ID)
 	if err != nil {
-		return []File{}[:]
+		return nil, err
 	}
 	defer rows.Close()
 
 	for i := 0; rows.Next(); i++ {
-		err := rows.Scan(&ret[i].Name, &ret[i].Path, &ret[i].UserID, &ret[i].AccessLevel)
+		var f File
+		err := rows.Scan(&f.Name, &f.Path, &f.UserID, &f.AccessLevel)
 		if err != nil {
-			return []File{}[:]
+			return nil, err
 		}
+		files = append(files, f)
 	}
 
-	return ret[0:count]
+	return files, nil
 }
 
 func GetFileByName(ID, name string) (File, error) {
@@ -95,7 +109,7 @@ func GetFileByName(ID, name string) (File, error) {
 }
 
 func InsertFileInfo(file File) error {
-	_, err := db.Exec("insert into files values(?, ?, ?, ?)", file.Name, file.Path, file.UserID, file.AccessLevel)
+	_, err := db.Exec("insert into files values(?, ?, ?, ?, ?)", file.Name, file.Path, file.UserID, file.AccessLevel, file.Mime)
 	if err != nil {
 		return err
 	}
@@ -160,28 +174,54 @@ func RenameFile(ID, filename, newFilename, newAccessLevel string) error {
 	return nil
 }
 
-func GetFilePath(ID, filename string) (string, error) {
-	rows, err := db.Query("select path from files where name = ? and (access_level = 'public' or access_level = 'internal' or access_level = 'private' and user_id = ?)", filename, ID)
+func GetFilePath(ID, filename string) (path, mime string, err error) {
+	rows, err := db.Query("select path, mime from files where name = ? and (access_level = 'public' or access_level = 'internal' or access_level = 'private' and user_id = ?)", filename, ID)
 	if err != nil {
-		return "", err
+		return
 	}
 	if !rows.Next() {
-		return "", errors.New("file not found")
+		err = errors.New("file not found")
+		return
 	}
-	var path string
-	rows.Scan(&path)
-	return path, nil
+	rows.Scan(&path, &mime)
+	return
 }
 
-func GetPublicFilePath(filename string) (string, error) {
-	rows, err := db.Query("select path from files where name = ? and access_level = 'public'", filename)
+func GetPublicFilePath(filename string) (path, mime string, err error) {
+	rows, err := db.Query("select path, mime from files where name = ? and access_level = 'public'", filename)
 	if err != nil {
-		return "", err
+		return
 	}
 	if !rows.Next() {
-		return "", errors.New("file not found")
+		err = errors.New("file not found")
+		return
 	}
-	var path string
-	rows.Scan(&path)
-	return path, nil
+	rows.Scan(&path, &mime)
+	return
+}
+
+func UpdateMime() {
+	rows, err := db.Query("select path from files")
+	if err != nil {
+		log.Fatal(err)
+	}
+	base := os.Getenv("UPLOAD_FILE_PATH")
+	for rows.Next() {
+		var path string
+		rows.Scan(&path)
+		p := filepath.Join(base, path)
+		bytes, err := ioutil.ReadFile(p)
+		if err != nil {
+			log.Fatal(err)
+		}
+		mime := http.DetectContentType(bytes)
+		_, err = db.Exec("update files set mime = ? where path = ?", mime, path)
+		if err != nil {
+			log.Fatal(err)
+		}
+		m, err := exec.Command("bash", "-c", "gzip -c "+p+" > "+p+".gz").CombinedOutput()
+		if err != nil {
+			log.Fatal(m, err)
+		}
+	}
 }
